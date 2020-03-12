ee7b5fcc30b9d48e566b21d6c21305c50be2fc96
Patrick Tschackert <ptschack@googlemail.com>
2020-03-12 14:29:41 +0100

    Implement python ;)

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "What is the velocity of an unladen African swallow?"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
